0237498d4689c915e80bb29e6257442cb94c2fd6
HYUNBIN-LEE\user <hyunbeen95@gmail.com>
2018-05-15 22:02:46 +0900

    Royal Straight Flush와 Straight Flush의 정의 & 같은 문양의 판별식

--- CheckResult.java
+++ CheckResult.java
@@ -20,4 +20,31 @@ public class CheckResult {
 		ArrayList<Integer> selCloverCardPattern = new ArrayList<Integer>();
 		ArrayList<Integer> selHeartCardPattern 	= new ArrayList<Integer>();
 		ArrayList<Integer> selDiaCardPattern 	= new ArrayList<Integer>();
+
+
+		// * 로열 스트레이트 플러시(Royal Straight Flush) *
+		// * 5장의 카드가 같은 문양과 함께 A부터 순서적으로 *
+		// * 나열되어 있는 것 *
+
+
+		// * 스트레이트 플러시 (Straight Flush) *
+		// * 5장의 카드가 같은 문양과 함께 A보다 낮은 순서로 *
+		// * 나열되어 있는 것 *
+
+		// 같은 문양이 몇 장있나 판단.
+		for (int i = 0; i < Constants.CARDNUM; i++) {
+			if (mc.getCardPattern(card[i]).equals(Constants.SPADE)) {
+				spadeNum++;
+				selSpadeCardPattern.add((Integer) mc.getCardNum(card[i]));
+			} else if (mc.getCardPattern(card[i]).equals(Constants.CLOVER)) {
+				cloverNum++;
+				selCloverCardPattern.add((Integer) mc.getCardNum(card[i]));
+			} else if (mc.getCardPattern(card[i]).equals(Constants.HEART)) {
+				heartNum++;
+				selHeartCardPattern.add((Integer) mc.getCardNum(card[i]));
+			} else if (mc.getCardPattern(card[i]).equals(Constants.DIAMOND)) {
+				diaNum++;
+				selDiaCardPattern.add((Integer) mc.getCardNum(card[i]));
+			}
+		}
 }
