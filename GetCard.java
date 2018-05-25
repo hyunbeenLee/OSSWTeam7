e044d9439c08d6ccc6b7a926054a75725521f328
qkrqudwn2 <38180176+qkrqudwn2@users.noreply.github.com>
2018-05-25 22:32:34 +0900

    Update GetCard.java
    카드 덱 추가 제작

--- GetCard.java
+++ GetCard.java
@@ -1,12 +1,21 @@
 import java.util.Random;
 
 public class GetCard{
+   public int[] mixCard() {
    int[] cardNum = new int[12];
    int[] cardShape = new int[3];
+   int[] retVal  = new int[14];
    
    Random random = new Random();
-   for(int i=0; i<13; i++){
+   for(int i=0; i<14; i++){
     cardNum[i] = random.nextInt(52) + 1;
     }
+   for (int i = 0; i < 14; i++) {
+		for (int j = 0; j < 14; j++) {
+			if( (i != j) && (cardNum[i] == cardNum[j]) ){
+				cardNum[j] = random.nextInt(52) + 1;
+			}
+		}
+	}
   }
 }
