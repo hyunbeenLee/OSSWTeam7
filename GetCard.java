7d269672758c6d35d0adba7159d98720f6f41b5d
qkrqudwn2 <38180176+qkrqudwn2@users.noreply.github.com>
2018-05-28 09:55:42 +0900

    Update GetCard.java

--- GetCard.java
+++ GetCard.java
@@ -2,7 +2,7 @@ import java.util.Random;
 
 public class GetCard{
    public int[] mixCard() {
-   int[] cardNum = new int[12];
+   int[] cardNum = new int[14];
    int[] cardShape = new int[3];
    int[] retVal  = new int[14];
    
@@ -17,5 +17,23 @@ public class GetCard{
 			}
 		}
 	}
+// cardNum은 오름차순으로 정렬된 카드 덱이다.
+// retVal 배열은 랜덤한 순서의 카드 덱이다.
+	   
+	   retVal = cardNum.clone();
+	   
+	   int[] userCard = new int[Constants.CARDNUM];
+	   int[] compCard = new int[Constants.CARDNUM];
+	   
+	   for(int j=0; j<7; j++){
+		if(j==0) System.out.print("COM_CARD :" + "\t" + mc.getCardPattern(cardNum[j]) + "\t" + mc.getCardNum(cardNum[j]) + "\t");
+		else System.out.print(mc.getCardPattern(cardNum[j]) + "\t" + mc.getCardNum(cardNum[j]) + "\t");
+		if(j==6) System.out.println();
+	   }
+	   for(int k=7; k<14; k++){
+		if(k==7) System.out.print("BSH_CARD :" + "\t" + mc.getCardPattern(cardNum[k]) + "\t" + mc.getCardNum(cardNum[k]) + "\t");
+		else System.out.print(mc.getCardPattern(cardNum[k]) + "\t" + mc.getCardNum(cardNum[k]) + "\t");
+		if(k==13) System.out.println();
+	   }
   }
 }
