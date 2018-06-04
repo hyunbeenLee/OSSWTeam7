0d7f77be160f574111d839517f124fd99e2149d4
qkrqudwn2 <38180176+qkrqudwn2@users.noreply.github.com>
2018-06-04 09:38:24 +0900

    Update BroadSocket.java

--- BroadSocket.java
+++ BroadSocket.java
@@ -1,44 +1,40 @@
-import java.io.IOException;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
-
 import javax.websocket.OnClose;
+import javax.websocket.OnError;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
-import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
-
-@ServerEndpoint("/broadcasting")
-public class Broadsocket {
-
-	private static Set<Session> clients = Collections
-			.synchronizedSet(new HashSet<Session>());
-
-	@OnMessage
-	public void onMessage(String message, Session session) throws IOException {
-		System.out.println(message);
-		synchronized (clients) {
-			// Iterate over the connected sessions
-			// and broadcast the received message
-			for (Session client : clients) {
-				if (!client.equals(session)) {
-					client.getBasicRemote().sendText(message);
-				}
-			}
-		}
-	}
-
-	@OnOpen
-	public void onOpen(Session session) {
-		// Add session to the connected sessions set
-		System.out.println(session);
-		clients.add(session);
-	}
-
-	@OnClose
-	public void onClose(Session session) {
-		// Remove session from the connected sessions set
-		clients.remove(session);
-	}
+  
+@ServerEndpoint("/websocket")
+public class websocket {
+    /***
+     * 웹 소켓이 연결되면 호출되는 이벤트
+     */
+    @OnOpen
+    public void handleOpen(){
+        System.out.println("client is now connected...");
+    }
+    /**
+     * 웹 소켓으로부터 메시지가 오면 호출되는 이벤트
+     */
+    @OnMessage
+    public String handleMessage(String message){
+        System.out.println("receive from client : "+message);
+        String replymessage = "echo "+message;
+        System.out.println("send to client : "+replymessage);
+        return replymessage;
+    }
+    /**
+     * 웹 소켓이 닫히면 호출되는 이벤트
+     */
+    @OnClose
+    public void handleClose(){
+        System.out.println("client is now disconnected...");
+    }
+    /**
+     * 웹 소켓이 에러가 나면 호출되는 이벤트
+     */
+    @OnError
+    public void handleError(Throwable t){
+        t.printStackTrace();
+    }
 }
